2ffffc0617c9cb6cfb1c139091cce0b4b21f7d51
pabitra <pabitram322@gmail.com>
2026-07-11 16:53:15 +0000

    add concurrency using wait groups nd goroutines

--- main.go
+++ main.go
@@ -3,23 +3,34 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 	"time"
 )
 
 func main() {
 	host := "scanme.nmap.org"
 	start := 20
-	end := 100
+	end := 500
+
+	var wg sync.WaitGroup
 
 	for port := start; port <= end; port++ {
-		address := fmt.Sprintf("%s:%d", host, port)
-		conn, err := net.DialTimeout("tcp", address, time.Second)
-		if err != nil {
-			fmt.Println("not open: ", port)
-			continue
-		}
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			address := fmt.Sprintf("%s:%d", host, port)
 
-		conn.Close()
-		fmt.Println("open: ", port)
+			conn, err := net.DialTimeout("tcp", address, time.Second)
+			if err != nil {
+				fmt.Printf("%d not open\n", port)
+				return
+			}
+			conn.Close()
+
+			fmt.Printf("%d port open\n", port)
+
+		}(port)
 	}
+	wg.Wait()
+	fmt.Println("Scan completed")
 }
