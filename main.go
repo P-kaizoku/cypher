83e64ee3d44521022eefc9928189664155a60052
pabitra <pabitram322@gmail.com>
2026-07-11 17:24:19 +0000

    add channels nd worker pool to scale the process

--- main.go
+++ main.go
@@ -7,30 +7,42 @@ import (
 	"time"
 )
 
+func worker(host string, ports <-chan int, wg *sync.WaitGroup) {
+	for port := range ports {
+		func() {
+			defer wg.Done()
+			address := fmt.Sprintf("%s:%d", host, port)
+			conn, err := net.DialTimeout("tcp", address, time.Second)
+			if err != nil {
+				fmt.Printf("%d close\n", port)
+				return
+			}
+
+			fmt.Printf("%d open\n", port)
+			conn.Close()
+		}()
+	}
+}
+
 func main() {
 	host := "scanme.nmap.org"
 	start := 20
 	end := 500
+	numWorkers := 600
 
+	ports := make(chan int, end-start+1)
 	var wg sync.WaitGroup
+
+	for range numWorkers {
+		go worker(host, ports, &wg)
+	}
 
 	for port := start; port <= end; port++ {
 		wg.Add(1)
-		go func(port int) {
-			defer wg.Done()
-			address := fmt.Sprintf("%s:%d", host, port)
-
-			conn, err := net.DialTimeout("tcp", address, time.Second)
-			if err != nil {
-				fmt.Printf("%d not open\n", port)
-				return
-			}
-			conn.Close()
-
-			fmt.Printf("%d port open\n", port)
-
-		}(port)
+		ports <- port
 	}
+
+	close(ports)
 	wg.Wait()
 	fmt.Println("Scan completed")
 }
